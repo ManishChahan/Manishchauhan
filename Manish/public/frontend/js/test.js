ec2c449bc609591764f006506dc510df25441f5d
ManishChahan <136166861+ManishChahan@users.noreply.github.com>
2023-06-10 16:50:13 +0530

    first commit

---
+++ Manish/public/frontend/js/test.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#p1").click(function(){
+    $("body").css("background-color", "yellow");
+  });
+  $("#p2").click(function(){
+    $("body").css("background-color", "red");
+  });
+  $("#p3").click(function(){
+    $("body").css("background-color", "blue");
+  });
+});
